2a78200bd50405ce3a21626ef59a3031be4cb240
AME45 <2231488391@qq.com>
2026-08-04 18:00:38 +0800

    用户登录，校验相关业务代码的开发

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -26,22 +29,23 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     public Result sendCode(String phone, HttpSession session) {
-        if(StrUtil.isNotBlank(phone)) {
+        if(RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("手机号格式错误");
         }
         String code = RandomUtil.randomNumbers(6);
         session.setAttribute("code", code);
+        log.debug("验证码：" + code);
         return Result.ok();
     }
 
     public Result login(LoginFormDTO loginForm, HttpSession session) {
         String phone = loginForm.getPhone();
-        if(StrUtil.isNotBlank(phone)) {
+        if(RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("手机号格式错误");
         }
         Object cacheCode = session.getAttribute("code");
         String code = loginForm.getCode();
-        if(cacheCode != null || !cacheCode.toString().equals(code)) {
+        if(cacheCode == null || !cacheCode.toString().equals(code)) {
             return Result.fail("验证码错误");
         }
 
@@ -49,7 +53,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         if(user == null) {
              user = createUserWithPhone(phone);
         }
-        session.setAttribute("user", user);
+        // 保存用户信息到 session
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        session.setAttribute("user", userDTO);
         return Result.ok();
     }
     private User createUserWithPhone(String phone) {
