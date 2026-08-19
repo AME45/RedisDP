3d91fc4152b6c88764e394b634c9cade2019a2fd
AME45 <2231488391@qq.com>
2026-08-19 18:04:18 +0800

    互斥锁，逻辑过期（击穿）的工具类，优惠券秒杀的乐观锁解决超卖问题，悲观锁（java内置锁）解决一人一单代码的开发

---
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.lang.UUID;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class SimpleRedisLock implements ILock{
+
+    private String name;
+    private StringRedisTemplate stringRedisTemplate;
+
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
+        this.name = name;
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
+
+    @Override
+    public boolean trylock(long timeoutSec) {
+        String value = ID_PREFIX + Thread.currentThread().getId();
+        String lockKey = KEY_PREFIX + name;
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, value, timeoutSec, TimeUnit.SECONDS);
+        return Boolean.TRUE.equals(success);
+    }
+
+
+
+    @Override
+    public void unlock() {
+        String lockKey = KEY_PREFIX + name;
+        //获取当前线程id
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        //从redis中查锁的线程id
+        String threadId2 = stringRedisTemplate.opsForValue().get(lockKey);
+        //判断线程id是否一致来判断该锁是否属于当前线程，若是，则可以释放锁
+        if (threadId.equals(threadId2)){
+            stringRedisTemplate.delete(lockKey);
+        }
+    }
+}
